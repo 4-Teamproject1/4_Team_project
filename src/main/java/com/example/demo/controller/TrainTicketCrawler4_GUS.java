92171803676c5f28cba6a5eb13f87be805b3a967
Gouiseong <rhdmltjd1324@gmail.com>
2024-04-23 09:50:26 +0900

    검색 후 리스트 첫번째 요소 찾기 성공 클릭하는중

--- src/main/java/com/example/demo/controller/TrainTicketCrawler4_GUS.java
+++ src/main/java/com/example/demo/controller/TrainTicketCrawler4_GUS.java
@@ -13,75 +13,106 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TrainTicketCrawler4_GUS {
-    public static void main(String[] args) {
-        // 크롬 드라이버 경로 설정
-        System.setProperty("webdriver.chrome.driver", "C:/work/chromedriver-win64 (1)/chromedriver-win64/chromedriver.exe");
+	public static void main(String[] args) {
+		// 크롬 드라이버 경로 설정
+		System.setProperty("webdriver.chrome.driver",
+				"C:/work/chromedriver-win64 (1)/chromedriver-win64/chromedriver.exe");
 
-        // WebDriver 인스턴스 생성
-        WebDriver driver = new ChromeDriver();
+		// WebDriver 인스턴스 생성
+		WebDriver driver = new ChromeDriver();
 
-        // 네이버 기차표 검색 페이지 URL
-        String url = "https://map.naver.com/p?c=15.00,0,0,0,dh";
+		// 네이버 기차표 검색 페이지 URL
+//        String url = "https://www.agoda.com/ko-kr/?site_id=1922887&tag=f7739694-dbb7-41bd-aa27-be7c942ce354&gad_source=1&device=c&network=g&adid=695827820287&rand=7846428391314568431&expid=&adpos=&aud=kwd-6927948326&gclid=Cj0KCQjwlZixBhCoARIsAIC745BvF5aSyMA_QOWdSeqBil67b7Xx3zExuBbJP1Y2QtJ0ehVU8kW6aX8aAl0HEALw_wcB&pslc=1&ds=suPN3WV%2Fo7g%2BbofO";
 
-        // 기차표 검색 페이지로 이동
-        driver.get(url);
-        // 검색창 요소 찾기
-        WebElement searchInput = driver.findElement(By.cssSelector("div.input_box input.input_search"));
+		// 아고다 검색 페이지 url
+		String url = "https://url.kr/prnsqx";
 
-        // WebDriverWait 인스턴스 생성
-        WebDriverWait wait_web = new WebDriverWait(driver, Duration.ofSeconds(10));
+		// 아고다 검색 페이지로 이동
+		driver.get(url);
 
-        // 검색창이 활성화될 때까지 기다림
-        WebElement activatedSearchInput = wait_web.until(ExpectedConditions.elementToBeClickable(searchInput));
+		// 검색창 요소 찾기
+		WebElement searchInput = driver.findElement(By.cssSelector("#autocomplete-box #textInput"));
 
-        // 검색어 입력
-        String searchText = "경기도 성남시 분당구 하오개로 323";
-        activatedSearchInput.sendKeys(searchText);
+		// WebDriverWait 인스턴스 생성
+		WebDriverWait wait_web = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // 엔터 입력 (검색 실행)
-        activatedSearchInput.sendKeys(Keys.ENTER);
+		// 검색창이 활성화될 때까지 기다림
+		WebElement activatedSearchInput = wait_web.until(ExpectedConditions.elementToBeClickable(searchInput));
 
+		// 검색어 입력
+		String searchText = "서울";
+		activatedSearchInput.sendKeys(searchText);
 
-        // 도착 버튼 찾기
-        WebElement arrivalButton = wait_web.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.btn_area div.btn_box.direction button.btn_goal")));
-        // 도착 버튼 클릭
-        arrivalButton.click();
-        
-        
-     // WebDriverWait를 사용하여 출발지 입력란이 활성화될 때까지 기다립니다.
-        WebElement searchInput_start = wait_web.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.search_input_wrap div.search_input input.input_search")));
-        // 검색창을 클릭하여 활성화합니다.
-        searchInput_start.click();
-        // 검색어 입력
-        String searchText_start = "대전광역시 서구 둔산로 52 미라클빌딩 3층";
-        // 검색어 입력란에 값을 입력합니다.
-        searchInput_start.sendKeys(searchText_start);
-        searchInput_start.sendKeys(Keys.ENTER);
+		// 엔터 입력 (검색 실행)
+		activatedSearchInput.sendKeys(Keys.ENTER);
 
+		// 검색 후 첫번째 요소 찾기
+//        WebElement firstLiElement = driver.findElement(By.cssSelector(".Popup__content .AutocompleteList li:first-child"));
 
-    
-        // 도착 버튼 찾기
-        WebElement arrivalButton2 = wait_web.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.search_btn_area button.btn_direction.search.active")));
-        // 도착 버튼 클릭
-        arrivalButton2.click();
-        
-        // 도착 버튼 찾기
-        WebElement reservat_Button = wait_web.until(ExpectedConditions.elementToBeClickable(By.cssSelector("ol.list_pubtrans_directions_step li.is_selected button.btn_pubtrans_reserve")));
-        // 도착 버튼 클릭
-        reservat_Button.click();
-        
-        
-       
+		   try {
+	            // ul의 첫 번째 li 요소를 찾습니다.
+	            WebElement firstLi = driver.findElement(By.cssSelector("div.Popup__content ul.AutocompleteList li:first-child"));
 
-        // 기차 시간표 데이터 크롤링
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement timetable = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='_3-5Oc']")));
-        List<WebElement> scheduleRows = timetable.findElements(By.xpath(".//ul[@class='_3tdEo']//li"));
-        for (WebElement row : scheduleRows) {
-            System.out.println(row.getText());
-        }
+	            // 요소가 존재하는지 확인합니다.
+	            if (firstLi != null) {
+	            	wait_web = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // WebDriver 종료
-        driver.quit();
-    }
+	        		// 검색창이 활성화될 때까지 기다림
+	        		activatedSearchInput = wait_web.until(ExpectedConditions.elementToBeClickable(firstLi));
+	                // 해당 요소를 클릭합니다.
+	                firstLi.click();
+	            } else {
+	                System.out.println("첫 번째 li 요소를 찾을 수 없습니다.1");
+	            }
+	        } catch (org.openqa.selenium.NoSuchElementException e) {
+	            // 요소를 찾을 수 없을 때 예외 처리합니다.
+	            System.out.println("첫 번째 li 요소를 찾을 수 없습니다.2");
+	        }
+		// ul 요소 내의 첫 번째 li 요소를 찾습니다.
+		WebElement firstLiElement = driver
+				.findElement(By.cssSelector(""));
+
+		wait_web = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		// 검색창이 활성화될 때까지 기다림
+		activatedSearchInput = wait_web.until(ExpectedConditions.elementToBeClickable(firstLiElement));
+
+		// 첫번째 요소 클릭
+		firstLiElement.click();
+
+		// WebDriverWait를 사용하여 출발지 입력란이 활성화될 때까지 기다립니다.
+		WebElement searchInput_start = wait_web.until(ExpectedConditions
+				.elementToBeClickable(By.cssSelector("div.search_input_wrap div.search_input input.input_search")));
+		// 검색창을 클릭하여 활성화합니다.
+		searchInput_start.click();
+		// 검색어 입력
+		String searchText_start = "대전광역시 서구 둔산로 52 미라클빌딩 3층";
+		// 검색어 입력란에 값을 입력합니다.
+		searchInput_start.sendKeys(searchText_start);
+		searchInput_start.sendKeys(Keys.ENTER);
+
+		// 도착 버튼 찾기
+		WebElement arrivalButton2 = wait_web.until(ExpectedConditions
+				.elementToBeClickable(By.cssSelector("div.search_btn_area button.btn_direction.search.active")));
+		// 도착 버튼 클릭
+		arrivalButton2.click();
+
+		// 도착 버튼 찾기
+		WebElement reservat_Button = wait_web.until(ExpectedConditions.elementToBeClickable(
+				By.cssSelector("ol.list_pubtrans_directions_step li.is_selected button.btn_pubtrans_reserve")));
+		// 도착 버튼 클릭
+		reservat_Button.click();
+
+		// 기차 시간표 데이터 크롤링
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement timetable = wait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='_3-5Oc']")));
+		List<WebElement> scheduleRows = timetable.findElements(By.xpath(".//ul[@class='_3tdEo']//li"));
+		for (WebElement row : scheduleRows) {
+			System.out.println(row.getText());
+		}
+
+		// WebDriver 종료
+		driver.quit();
+	}
 }
