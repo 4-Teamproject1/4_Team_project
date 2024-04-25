68cd7d36804aae6b7e539e82e9e7b639d79d4aa4
Gouiseong <rhdmltjd1324@gmail.com>
2024-04-25 16:36:28 +0900

    아고다 크롤링(4), 검색 조건 넣고 검색버튼 클릭

--- src/main/java/com/example/demo/controller/TrainTicketCrawler4_GUS.java
+++ src/main/java/com/example/demo/controller/TrainTicketCrawler4_GUS.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -25,11 +27,9 @@ public class TrainTicketCrawler4_GUS {
 		// WebDriver 인스턴스 생성
 		WebDriver driver = new ChromeDriver();
 
-		// 네이버 기차표 검색 페이지 URL
-//        String url = "https://www.agoda.com/ko-kr/?site_id=1922887&tag=f7739694-dbb7-41bd-aa27-be7c942ce354&gad_source=1&device=c&network=g&adid=695827820287&rand=7846428391314568431&expid=&adpos=&aud=kwd-6927948326&gclid=Cj0KCQjwlZixBhCoARIsAIC745BvF5aSyMA_QOWdSeqBil67b7Xx3zExuBbJP1Y2QtJ0ehVU8kW6aX8aAl0HEALw_wcB&pslc=1&ds=suPN3WV%2Fo7g%2BbofO";
-
 		// 아고다 검색 페이지 url
-		String url = "https://url.kr/prnsqx";
+		String url = "https://www.agoda.com/ko-kr/?site_id=1922887&tag=f7739694-dbb7-41bd-aa27-be7c942ce354&gad_source=1&device=c&network=g&adid=695827820287&rand=7846428391314568431&expid=&adpos=&aud=kwd-6927948326&gclid=Cj0KCQjwlZixBhCoARIsAIC745BvF5aSyMA_QOWdSeqBil67b7Xx3zExuBbJP1Y2QtJ0ehVU8kW6aX8aAl0HEALw_wcB&pslc=1&ds=rsjlTo6c2PdQF7zG";
+
 
 		// 아고다 검색 페이지로 이동
 		driver.get(url);
@@ -74,13 +74,7 @@ public class TrainTicketCrawler4_GUS {
 		// 첫 번째 요소 클릭
 		firstElement.click();
 
-		System.out.println("asd");
-
-		// 원하는 날짜 설정
-		String targetDate = "Thu May 02 2024";
-
-		// 웹 요소 찾기
-
+		// 페이지 왼쪽 달력 년도하고 달
 		String monthStr = "//div[contains(@class,'DayPicker-Caption') and contains(@class,'DayPicker-Caption-Wide')]";
 
 		List<WebElement> monthElements = wait_web
@@ -90,15 +84,13 @@ public class TrainTicketCrawler4_GUS {
 //		for (WebElement element : monthElements) {
 //			System.out.println("텍스트 값: " + element.getText());
 //		}
-		// 기본 달 값
-		System.out.println(monthElements.get(0).getText());
 
 		// monthElements.get(0).getText()로 얻어온 텍스트
-		String monthText = monthElements.get(0).getText();
+		String pageYearMonth = monthElements.get(0).getText();
 
 		// 년도와 월을 추출할 정규 표현식 패턴 설정
 		Pattern pattern = Pattern.compile("(\\d{4})년 (\\d{1,2})월");
-		Matcher matcher = pattern.matcher(monthText);
+		Matcher matcher = pattern.matcher(pageYearMonth);
 
 		int pageYear = 0;
 		int pageMonth = 0;
@@ -128,39 +120,74 @@ public class TrainTicketCrawler4_GUS {
 		// 합친 년도와 달 문자열 생성
 		String currentYearMonth = nowYear + "년 " + nowMonth + "월";
 
-		System.out.println(currentYearMonth);
-		System.out.println(monthElements.get(0).getText());
-		if (currentYearMonth.equals(monthElements.get(0).getText())) {
-
-			System.out.println("현재 달과 일치");
-		} else if (pageYear == nowYear && pageMonth > nowMonth) {
-			previousMonthButton.click();
-
-		} else if (pageYear == nowYear && pageMonth < nowMonth) {
-			nextMonthButton.click();
-
-		} else {
-			System.out.println("값이 일치하지 않음");
-		}
-
-		String calendarElementsStr = "//div[@class='DayPicker-Week-Wide']";
-		List<WebElement> calendarElements = wait_web
-				.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(calendarElementsStr)));
-
-		for (WebElement element : calendarElements) {
-			System.out.println(element.getText());
+//		현재날짜와 페이지날짜 출력
+//		System.out.println(currentYearMonth);
+//		System.out.println(monthElements.get(0).getText());
+		if (currentYearMonth == pageYearMonth) {
 
 		}
+		while (true) {
+
+			if (pageYear == nowYear && pageMonth > nowMonth) {
+				previousMonthButton.click();
+				break;
+
+			} else {
+				System.out.println("값이 일치하지 않음");
+				break;
+			}
+
+		}
+		// 날짜 포맷 지정
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd yyyy", Locale.ENGLISH);
+
+		// 현재 날짜를 원하는 형식으로 파싱
+		// 이걸로 날짜 찾아서 선택해줄예정
+		String startDate = currentDate.format(formatter);
+		
+		// 현재 날짜 요소 찾기
+		String startDateStr = "//div[@class='DayPicker-Week-Wide']/div[contains(@aria-label, '" + startDate + "')]";
+		WebElement startDateElement= driver.findElement(By.xpath(startDateStr));
+		
+		// 찾은 현재 날짜 클릭(시작 날짜)
+		startDateElement.click();
+		
+		 // 다음날 날짜 구하기
+        LocalDate plusNowDate = currentDate.plusDays(1);
+        
+        String nextDate =plusNowDate.format(formatter);
+        String endDateStr = "//div[@class='DayPicker-Week-Wide']/div[contains(@aria-label, '" + nextDate + "')]";
+        WebElement endDateElement= driver.findElement(By.xpath(endDateStr));
+		
+        endDateElement.click();
+	
+        // 버튼 요소를 포함하는 div 요소의 XPath
+        String buttonXPathStr = "//div[@class='Box-sc-kv6pi1-0 hRUYUu TabContent__Search--button']//button[@data-test='SearchButtonBox']";
+
+        // 버튼 요소 찾기
+        WebElement buttonElement = driver.findElement(By.xpath(buttonXPathStr));
+        
+        // 검색하기 클릭
+        buttonElement.click();
+
+//		String calendarElementsStr = "//div[@class='DayPicker-Week-Wide']";
+//		List<WebElement> calendarElements = wait_web
+//				.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(calendarElementsStr)));
+//
+//		for (WebElement element : calendarElements) {
+//			System.out.println(element.getText());
+//
+//		}
 
 //		 // div 안에 있는 값으로 요소 찾기
 //	        String divText = "5";
 //	        WebElement elementInsideDiv = driver.findElement(By.xpath("//div[text()='" + divText + "']"));
 
 //		 startDay.click();
-		System.out.println("asd");
+		
 
-		WebElement endDay = driver.findElement(By.xpath("//div[@aria-label='Wed May 04 2024 ']"));
-
-		endDay.click();
+//		WebElement endDay = driver.findElement(By.xpath("//div[@aria-label='Wed May 04 2024 ']"));
+//
+//		endDay.click();
 	}
 }
