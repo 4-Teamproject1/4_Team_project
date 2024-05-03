5e9be01628c3b4879669ebe9d4c1f0fb4f967c6f
JUNGmoon123 <jungpro567@gmail.com>
2024-05-03 20:26:09 +0900

    JDW복원중

--- src/main/java/com/example/demo/repository/ConferenceRepository.java
+++ src/main/java/com/example/demo/repository/ConferenceRepository.java
@@ -15,13 +15,7 @@ public interface ConferenceRepository {
 	@Insert("""
 			INSERT INTO `academy` SET
 			         categoryId = #{categoryId},
-<<<<<<< HEAD
-			         themeId = 1,
 			         title = #{title},
-=======
-			         title = #{title},
-			         themeId = 1,
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 			         hitCount = #{hitCount},
 			         eventPeriod = #{eventPeriod},
 			         applicationPeriod = #{applicationPeriod},
@@ -35,14 +29,6 @@ public interface ConferenceRepository {
 	public void insertConference(Conference conference);
 
 	@Select("""
-<<<<<<< HEAD
-			<script>
-			SELECT *
-			FROM `academy`;
-			</script>
-			""")
-	public List<Conference> getShopsList();
-=======
 		    <script>
 		    SELECT * 
 		    FROM `academy` 
@@ -59,147 +45,77 @@ public interface ConferenceRepository {
 			</script>
 			""")
 	public Conference getEventById(int id);
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 
 	@Select("""
 			<script>
 			SELECT *
 			FROM `academy`
-<<<<<<< HEAD
-			WHERE id = #{id};
-			</script>
-			""")
-	public Conference getEventById(int id);
-=======
 			WHERE categoryId = #{categoryId};
 			</script>
 			""")
 	public List<Conference> getConferencesByCategory(int categoryId);
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 
 	@Select("""
 			<script>
 			SELECT *
 			FROM `academy`
-<<<<<<< HEAD
-			WHERE categoryId = #{categoryId};
-			</script>
-			""")
-	public List<Conference> getConferencesByCategory(int categoryId);
-=======
 			ORDER BY STR_TO_DATE(regDate, '%Y-%m-%d') DESC
 			</script>
 			""")
 	public List<Conference> getConferencesOrderByRegDate();
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 
 	@Select("""
 			<script>
 			SELECT *
 			FROM `academy`
-<<<<<<< HEAD
-			ORDER BY STR_TO_DATE(regDate, '%Y-%m-%d') DESC
-			</script>
-			""")
-	public List<Conference> getConferencesOrderByRegDate();
-=======
 			WHERE categoryId = #{categoryId}
 			ORDER BY STR_TO_DATE(regDate, '%Y-%m-%d') DESC
 			</script>
 			""")
 	public List<Conference> getConferencesByCategoryOrderByRegDate(int categoryId);
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 
 	@Select("""
 			<script>
 			SELECT *
 			FROM `academy`
-<<<<<<< HEAD
-			WHERE categoryId = #{categoryId}
-			ORDER BY STR_TO_DATE(regDate, '%Y-%m-%d') DESC
-			</script>
-			""")
-	public List<Conference> getConferencesByCategoryOrderByRegDate(int categoryId);
-=======
 			ORDER BY hitCount DESC
 			</script>
 			""")
 	public List<Conference> getConferencesOrderByhitCount();
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 
 	@Select("""
 			<script>
 			SELECT *
 			FROM `academy`
-<<<<<<< HEAD
-			ORDER BY hitCount DESC
-			</script>
-			""")
-	public List<Conference> getConferencesOrderByhitCount();
-=======
 			WHERE categoryId = #{categoryId}
 			ORDER BY hitCount DESC
 			</script>
 			""")
 	public List<Conference> getConferencesByCategoryOrderByhitCount(int categoryId);
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 
 	@Select("""
 			<script>
 			SELECT *
 			FROM `academy`
-<<<<<<< HEAD
-			WHERE categoryId = #{categoryId}
-			ORDER BY hitCount DESC
-			</script>
-			""")
-	public List<Conference> getConferencesByCategoryOrderByhitCount(int categoryId);
-=======
 			ORDER BY STR_TO_DATE(SUBSTRING_INDEX(applicationPeriod, ' ~ ', -1), '%y.%m.%d') ASC
 			</script>
 			""")
 	public List<Conference> getConferencesOrderByfinDate();
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 
 	@Select("""
 			<script>
 			SELECT *
 			FROM `academy`
-<<<<<<< HEAD
-			ORDER BY STR_TO_DATE(SUBSTRING_INDEX(applicationPeriod, ' ~ ', -1), '%y.%m.%d') ASC
-			</script>
-			""")
-	public List<Conference> getConferencesOrderByfinDate();
-=======
 			WHERE categoryId = #{categoryId}
 			ORDER BY STR_TO_DATE(SUBSTRING_INDEX(applicationPeriod, ' ~ ', -1), '%y.%m.%d') ASC
 			</script>
 			""")
 	public List<Conference> getConferencesByCategoryOrderByfinDate(int categoryId);
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 
 	@Select("""
 			<script>
 			SELECT *
 			FROM `academy`
-<<<<<<< HEAD
-			WHERE categoryId = #{categoryId}
-			ORDER BY STR_TO_DATE(SUBSTRING_INDEX(applicationPeriod, ' ~ ', -1), '%y.%m.%d') ASC
-			</script>
-			""")
-	public List<Conference> getConferencesByCategoryOrderByfinDate(int categoryId);
-=======
-			ORDER BY title ASC
-			</script>
-			""")
-	public List<Conference> getConferencesOrderBytitle();
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
-
-	@Select("""
-			<script>
-			SELECT *
-			FROM `academy`
-<<<<<<< HEAD
 			ORDER BY title ASC
 			</script>
 			""")
@@ -216,15 +132,6 @@ public interface ConferenceRepository {
 	public List<Conference> getConferencesByCategoryOrderBytitle(int categoryId);
 
 	@Select("""
-=======
-			WHERE categoryId = #{categoryId}
-			ORDER BY title ASC
-			</script>
-			""")
-	public List<Conference> getConferencesByCategoryOrderBytitle(int categoryId);
-
-	@Select("""
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 			    <script>
 			    SELECT title,place,
 			           CONCAT(
@@ -238,11 +145,7 @@ public interface ConferenceRepository {
 			""")
 	public List<Conference> getShopsList2();
 
-<<<<<<< HEAD
-	@Update("""
-=======
     @Update("""
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
 			UPDATE `academy`
 			SET goodReactionPoint = goodReactionPoint + 1
 			WHERE id = #{academyId}
@@ -250,15 +153,12 @@ public interface ConferenceRepository {
 			""")
 	public int increaseGoodReactionPoint(int academyId, int themeId);
 
-	@Update("""
+    @Update("""
 			UPDATE `academy`
 			SET goodReactionPoint = goodReactionPoint - 1
 			WHERE id = #{academyId}
-			AND themeId = #{themeId}
+			AND themeId = #{themeId} 
 			""")
-<<<<<<< HEAD
-	public int decreaseGoodReactionPoint(int themeId, int academyId);
-=======
 	public int decreaseGoodReactionPoint(int academyId, int themeId);
 
     
@@ -268,23 +168,9 @@ public interface ConferenceRepository {
     	    WHERE id = #{academyId}
     	    AND themeId = #{themeId}
     	    """)
-    	public int getGoodRP(int themeId , int academyId);
+    	public int getGoodRP(int themeId, int academyId);
 
-
-
-
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
-
-	@Select("""
-			SELECT goodReactionPoint
-			FROM `academy`
-			WHERE id = #{academyId}
-			AND themeId = #{themeId}
-			""")
-	public int getGoodRP(int themeId, int academyId);
-
-	
-	@Select("""
+    @Select("""
 		    <script>
 		    SELECT title, place,
 		           DATE_FORMAT(SUBSTRING_INDEX(eventPeriod, ' ~ ', 1), '%y.%m.%d') AS eventPeriod,
@@ -300,5 +186,9 @@ public interface ConferenceRepository {
 		public List<Conference> getscrapShopsList(int memberId);
 
 
+
+	
+
+   
 
 }
--- src/main/java/com/example/demo/repository/ScrapRepository.java
+++ src/main/java/com/example/demo/repository/ScrapRepository.java
@@ -8,56 +8,52 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface ScrapRepository {
 
-	@Select("""
-			SELECT IFNULL(SUM(point),0)
-			FROM scrap
-			WHERE academyId = #{academyId}
-			AND memberId = #{loginedMemberId}
-			AND themeId = #{themeId}
-			""")
-<<<<<<< HEAD
-	public int getSumReactionPoint(int loginedMemberId, int themeId, int academyId);
-=======
-	public int getSumReactionPoint(int academyId, int memberId, int themeId);
->>>>>>> 6eb69d3e7d42d5cb46f3b26b428f9ae1e4bad0d6
+   @Select("""
+         SELECT IFNULL(SUM(point),0)
+         FROM scrap
+         WHERE academyId = #{academyId}
+         AND memberId = #{loginedMemberId}
+         AND themeId = #{themeId}
+         """)
+   public int getSumReactionPoint(int loginedMemberId, int themeId, int academyId);
 
-	@Insert("""
-			INSERT INTO scrap
-			SET regDate = NOW(),
-			updateDate = NOW(),
-			academyId = #{academyId},
-			memberId = #{memberId},
-			themeId = #{themeId},
-			`point` = 1
-			""")
-	public int addGoodReactionPoint(int memberId, int themeId, int academyId);
+   @Insert("""
+         INSERT INTO scrap
+         SET regDate = NOW(),
+         updateDate = NOW(),
+         academyId = #{academyId},
+         memberId = #{memberId},
+         themeId = #{themeId},
+         `point` = 1
+         """)
+   public int addGoodReactionPoint(int memberId, int themeId, int academyId);
 
-	/*
-	 * @Insert(""" INSERT INTO scrap SET regDate = NOW(), updateDate = NOW(), relId
-	 * = #{relId}, memberId = #{memberId}, `point` = -1 """) public int
-	 * addBadReactionPoint(int memberId, String relTypeCode, int relId);
-	 */
+   /*
+    * @Insert(""" INSERT INTO scrap SET regDate = NOW(), updateDate = NOW(), relId
+    * = #{relId}, memberId = #{memberId}, `point` = -1 """) public int
+    * addBadReactionPoint(int memberId, String relTypeCode, int relId);
+    */
 
-	@Delete("""
-			DELETE FROM scrap
-			WHERE memberId = #{memberId}
-			AND academyId = #{academyId}
-			AND themeId = #{themeId}
-			""")
-	public void deleteReactionPoint(int memberId, int themeId,  int academyId );
+   @Delete("""
+         DELETE FROM scrap
+         WHERE memberId = #{memberId}
+         AND academyId = #{academyId}
+         AND themeId = #{themeId}
+         """)
+   public void deleteReactionPoint(int memberId, int themeId,  int academyId );
 
-	
-	
-	@Select("""
-			SELECT IFNULL(SUM(S.point),0)
-			FROM scrap AS S
-			WHERE S.Id = #{id}
-			AND S.memberId =#{loginedMemberId}
-			AND S.themeId =#{themeId}
-			""")
-	public int getSumScrapCount(int loginedMemberId, int id, int themeId);
+   
+   
+   @Select("""
+         SELECT IFNULL(SUM(S.point),0)
+         FROM scrap AS S
+         WHERE S.Id = #{id}
+         AND S.memberId =#{loginedMemberId}
+         AND S.themeId =#{themeId}
+         """)
+   public int getSumScrapCount(int loginedMemberId, int id, int themeId);
 
-	
-	
+   
+   
 
 }
